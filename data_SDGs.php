e43b6bc4cd1591063a6b6754e880be3f38b877b2
HariandoMuthi <104293336+hariandoMuthi@users.noreply.github.com>
2023-05-26 18:35:31 +0700

    Add files via upload

---
+++ data_SDGs.php
@@ -0,0 +1,65 @@
+<?php include "header.php"; ?>
+<!-- start banner Area -->
+<section class="sdgs-banner relative">
+    <div class="overlay overlay-bg"></div>
+    <div class="container">
+        <div class="row d-flex align-items-center justify-content-center">
+            <div class="about-content col-lg-12">
+                <h1 class="text-white">
+                    Sustainable Deveopment <br> Goals (SDGs)
+                </h1>
+                <p class="text-white link-nav">Halaman ini memuat informasi Sustainable Deveopment Goals (SDGs)</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End banner Area -->
+<!-- Start about-info Area -->
+<section class="about-info-area section-gap">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h4>SDGs</h4> <br>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 text-justify">
+                Tujuan Pembangunan Berkelanjutan (TPB)/ Sustainable Development Goals (SDGs) adalah pembangunan yang
+                menjaga peningkatan kesejahteraan ekonomi masyarakat secara berkesinambingan, pembangunan yang menjaga
+                keberlanjutan kehidupan sosial masyarakat, pembangunan yang menjaga kualitas lingkungan hidup serta
+                pembangunan yang menjamin keadilan dan terlaksananya tata kelola yang mampu menjaga peningkatan kualitas
+                hidup dari satu generasi ke generasi berikutnya.
+            </div>
+            <div class="col-6 sdgs-gambar1">
+            </div>
+        </div>
+    </div>
+</section>
+<section class="about-info-area section-gap">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h4>Provinsi Riau Dalam SDG’s</h4><br>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 sdgs-gambar2">
+            </div>
+            <div class="col-6 text-justify">
+                Riau adalah sebuah provinsi di Indonesia yang terletak di bagian tengah pantai timur pulau Sumatra.
+                Wilayah pesisirnya berbatasan dengan Selat Malaka. Hingga tahun 2004, provinsi ini juga meliputi
+                Kepulauan Riau, sekelompok besar pulau-pulau kecil yang terletak di sebelah Timur Sumatra dan sebelah
+                Selatan Singapura.
+                Tujuan SDG’s 15 tentang ekosistem darat, yang secara khusus memandatkan pengelolaan hutan secara
+                lestari, menghentikan penggurunan, memulihkan degradasi lahan serta menghentikan kehilangan
+                keanekaragaman hayati.
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End about-info Area -->
+<?php include "footer.php"; ?>
